14a8757f48f84016523d54f1fc88a3c437ad47c1
Imagining_Time_and_Space <zehuili200756@gmail.com>
2026-05-28 14:55:55 +0800

    修正头文件引用

--- cpufreq_lse.c
+++ cpufreq_lse.c
@@ -15,7 +15,7 @@
 #include <linux/cpufreq.h>
 #include <linux/sched.h>
 #include <linux/sched/cputime.h>
-#include <linux/sched/types.h>
+#include <uapi/linux/sched/types.h>
 #include <trace/hooks/sched.h>
 
 #include "lse_main.h"
